428e1fea5226034550b30c7ec6e576ddb0203af0
mohsinghailan <mo.ghailan@gmail.com>
2019-04-11 12:35:49 +0200

    Variables_funciones

---
+++ 11_var/JS/app.js
@@ -0,0 +1,10 @@
+/* LENGUAJE JS */
+/* las variables son los elementos que utilizamos para almacenar la información. 
+Es un nombre al que se le asigna un valor. Let quiere decir que voy a crear una variable */
+
+let x = 12
+let y = 3
+let suma 
+suma = x + y
+console.log(suma)
+console.dir(suma)
